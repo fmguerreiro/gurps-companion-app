ce3f2d35f4a9a1424969ad39e8f047907c2f2520
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2024-03-06 10:26:56 +0900

    fix: style of top label content

--- src/gurps/pages/character/stats.cljs
+++ src/gurps/pages/character/stats.cljs
@@ -19,31 +19,35 @@
 (defn character-stats-page [^js props]
   (r/with-let [counter (rf/subscribe [:get-counter])
                tap-enabled? (rf/subscribe [:counter-tappable?])]
-    [:> styled-view {:className "flex-1 py-8 bg-white items-center justify-between"}
 
-     [summary]
+    [:> rn/ScrollView
+     [:> styled-view {:className "flex flex-col gap-4 py-4 bg-white items-center justify-between"}
 
-     [:> styled-view {:className "flex flex-row gap-0"}
-      [:> styled-view {:className "flex flex-column"}
-       [stat-label {:label (i18n/label :t/stat-strength) :value 12 :cost 20 :add-current-space? false}]
-       [stat-label {:label (i18n/label :t/stat-dexterity) :value 12 :cost 20 :add-current-space? false}]
-       [stat-label {:label (i18n/label :t/stat-intelligence) :value 12 :cost 20 :add-current-space? false}]
-       [stat-label {:label (i18n/label :t/stat-health) :value 12 :cost 20 :add-current-space? false}]]
-      [:> styled-view {:className "flex flex-column"}
-       [stat-label {:label (i18n/label :t/stat-hitpoints) :value 12 :current 11 :cost 20}]
-       [stat-label {:label (i18n/label :t/stat-will) :value 12 :cost 20}]
-       [stat-label {:label (i18n/label :t/stat-perception) :value 12 :cost 20}]
-       [stat-label {:label (i18n/label :t/stat-fatigue) :value 12 :current 11 :cost 20}]]]
+      [:> styled-view {:className "w-full px-4"}
+       [summary]]
 
-     [:> styled-view {:className "items-center gap-y-4"}
-      [:> styled-text {:className "text-4xl color-blue-500 font-bold"} @counter]
-      [button {:on-press #(rf/dispatch [:inc-counter]) :disabled? (not @tap-enabled?) :style {:background-color :red}} "Tap me, I'll count"]]
-     [:> rn/View {:style {:align-items :center}}
-      [button {:on-press (fn [] (-> props .-navigation (.navigate (i18n/label :t/items))))} "Tap me, I'll navigate"]]
-     [:> rn/View
+      [:> styled-view {:className "flex flex-row gap-0"}
+       [:> styled-view {:className "flex flex-col"}
+        [stat-label {:label (i18n/label :t/stat-strength) :value 12 :cost 20 :add-current-space? false}]
+        [stat-label {:label (i18n/label :t/stat-dexterity) :value 12 :cost 20 :add-current-space? false}]
+        [stat-label {:label (i18n/label :t/stat-intelligence) :value 12 :cost 20 :add-current-space? false}]
+        [stat-label {:label (i18n/label :t/stat-health) :value 12 :cost 20 :add-current-space? false}]]
+       [:> styled-view {:className "flex flex-col"}
+        [stat-label {:label (i18n/label :t/stat-hitpoints) :value 12 :current 11 :cost 20}]
+        [stat-label {:label (i18n/label :t/stat-will) :value 12 :cost 20}]
+        [stat-label {:label (i18n/label :t/stat-perception) :value 12 :cost 20}]
+        [stat-label {:label (i18n/label :t/stat-fatigue) :value 12 :current 11 :cost 20}]]]
+
+      [:> styled-view {:className "items-center gap-y-4"}
+       [:> styled-text {:className "text-4xl color-blue-500 font-bold"} @counter]
+       [button {:on-press #(rf/dispatch [:inc-counter]) :disabled? (not @tap-enabled?) :style {:background-color :red}} "Tap me, I'll count"]]
+      [:> rn/View {:style {:align-items :center}}
+       [button {:on-press (fn [] (-> props .-navigation (.navigate (i18n/label :t/items))))} "Tap me, I'll navigate"]]
+      [:> rn/View
       ;; [:> styled-view {:className "flex-1 items-center gap-y-4"}
       ;;  [:> rn/Image {:className "w-8 h-8" :source cljs-splash}]
       ;;  [:> rn/Image {:className "w-8 h-8" :source shadow-splash}]]
-      [:> styled-text {:style {:font-weight :normal :font-size 15 :color :blue}}
-       "Using: shadow-cljs+expo+reagent+re-frame"]]
-     [:> StatusBar {:style "auto"}]]))
+       [:> styled-text {:style {:font-weight :normal :font-size 15 :color :blue}}
+        "Using: shadow-cljs+expo+reagent+re-frame"]]
+
+      [:> StatusBar {:style "auto"}]]]))
--- src/gurps/pages/character/widgets/summary.cljs
+++ src/gurps/pages/character/widgets/summary.cljs
@@ -3,28 +3,29 @@
             [gurps.widgets.base :refer [styled-view styled-text styled-input]]))
 
 (defn- labelled-input
-  [label]
-  [:> styled-view {:className "flex flex-row gap-2 flex-auto justify-items-start"}
-   [:> styled-text {:className "font-bold"} label]
-   [:> styled-input {:className (str "bg-red-100 grow")}]])
+  ([label] (labelled-input label "max"))
+  ([label max-size]
+   [:> styled-view {:className "flex flex-row gap-2 justify-items-start flex-grow"}
+    [:> styled-text {:className "font-bold"} label]
+    [:> styled-input {:className (str "flex-grow border-b-2")}]]))
 
 (defn- input-group
   [children]
-    [:> styled-view {:className "gap-1 flex flex-row items-stretch bg-blue-500"}
-     children])
+  [:> styled-view {:className "gap-1 flex flex-row items-stretch"}
+   children])
 
 (defn summary
   [^js {}]
-  [:> styled-view {:className "flex flex-col gap-1 bg-red-500"}
+  [:> styled-view {:className "flex flex-col"}
    (input-group [:<>
                  (labelled-input (i18n/label :t/name))
                  (labelled-input (i18n/label :t/player))
-                 (labelled-input (i18n/label :t/point-total))])
+                 (labelled-input (i18n/label :t/point-total) 2)])
    (input-group [:<>
-                 (labelled-input (i18n/label :t/ht))
-                 (labelled-input (i18n/label :t/wt))
-                 (labelled-input (i18n/label :t/size-modifier))
-                 (labelled-input (i18n/label :t/age))
-                 (labelled-input (i18n/label :t/unspent-points))])
-   (input-group (labelled-input (i18n/label :t/appearance)))
-   ])
+                 (labelled-input (i18n/label :t/ht) 2)
+                 (labelled-input (i18n/label :t/wt) 2)
+                 (labelled-input (i18n/label :t/size-modifier) 2)
+                 (labelled-input (i18n/label :t/unspent-points) 2)])
+   (input-group [:<>
+                 (labelled-input (i18n/label :t/age) 2)
+                 (labelled-input (i18n/label :t/appearance))])])
