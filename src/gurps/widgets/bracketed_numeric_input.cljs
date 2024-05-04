1186a8c6eeeff8550f8a86c3caf26e5e4a110881
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2024-05-04 10:42:00 +0900

    style: center text in bracketed numeric input

--- src/gurps/widgets/bracketed_numeric_input.cljs
+++ src/gurps/widgets/bracketed_numeric_input.cljs
@@ -4,16 +4,18 @@
             ["twrnc" :refer [style] :rename {style tw}]))
 
 (defn bracketed-numeric-input
-  [{:keys [on-change-text val editable?]}]
+  [{:keys [on-change-text val editable?]
+    :or   {editable? true}}]
   [:> view {:style (tw "flex flex-1 flex-row items-center justify-center")}
    [:> text {:style (tw "text-xl font-bold")} "["]
 
    (if editable?
-     [:> input {:style (tw "text-xl font-bold bg-slate-200 min-w-6 text-center")
+     [:> input {:style (tw "text-xl font-bold min-w-6 pb-1 bg-slate-200")
                 :maxLength 3
                 :keyboardType "numeric"
+                :textAlign "center"
                 :onChangeText (debounce/debounce #(on-change-text %) 500)
-                :placeholder val}]
+                :placeholder (str val)}]
      [:> text {:style (tw "text-xl font-bold min-w-6 text-center")} val])
 
    [:> text {:style (tw "text-xl font-bold")} "]"]])
