11ba87837bfebb7d6b1cf205696c2fda3a94aff6
Filipe Guerreiro <filipe.m.guerreiro@gmail.com>
2024-06-23 14:09:24 +0900

    feat(advantage): navigate to owned advs after purchase

--- src/gurps/pages/character/advantages/details.cljs
+++ src/gurps/pages/character/advantages/details.cljs
@@ -1,5 +1,6 @@
 (ns gurps.pages.character.advantages.details
-  (:require ["twrnc" :refer [style] :rename {style tw}]
+  (:require ["@react-navigation/native" :as rnn]
+            ["twrnc" :refer [style] :rename {style tw}]
             [cljs-bean.core :refer [->clj]]
             [gurps.pages.character.utils.advantages :refer [advantages-by-name]]
             [gurps.pages.character.utils.disadvantages :refer [disadvantages-by-name]]
@@ -15,11 +16,10 @@
    [:> text {:style (tw "font-bold capitalize")} label]
    children])
 
-(:reprogrammable disadvantages-by-name)
-
 (defn advantage-details-page
   [props type]
-  (let [id         (-> props ->clj :route :params :id str->key)
+  (let [nav        (rnn/useNavigation)
+        id         (-> props ->clj :route :params :id str->key)
         by-name    (if (= type :advantages) advantages-by-name disadvantages-by-name)
         advantages (some-> (rf/subscribe [type]) deref)]
     [:> view {:style (tw "bg-white flex flex-col grow")}
@@ -37,4 +37,5 @@
 
      (when (not (id advantages))
        [:> view {:style (tw "absolute bottom-4 right-4")}
-        [add-button {:on-click #(rf/dispatch [(keyword type :add) id (id by-name)])}]])]))
+        [add-button {:on-click #(do (rf/dispatch [(keyword type :add) id (id by-name)])
+                                    (-> nav (.navigate (i18n/label (keyword :t (str "character-" type))))))}]])]))
